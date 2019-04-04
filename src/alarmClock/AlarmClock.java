46496617aac5fb9861130d88128de81b8f010645
Slama, Aaron <amslama@ilstu.edu>
2019-04-04 14:02:30 -0500

    Added base framework for gui

--- src/alarmClock/AlarmClock.java
+++ src/alarmClock/AlarmClock.java
@@ -1,10 +1,34 @@
 package alarmClock;
 
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class AlarmClock {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Creates GUI
+		JFrame mainGUI = new JFrame("Alarm Clock");
+		mainGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainGUI.setSize(600,200);
+		mainGUI.setLocationRelativeTo(null);
 
+		mainGUI.add(createGui());
+		mainGUI.setVisible(true);
+	}
+	
+	public static JPanel createGui() 
+	{
+		
+		JPanel mainGui = new JPanel();
+		
+		
+		
+		
+		return mainGui;
 	}
 
 }
