99adf03c7198c57499a619908966d4f299f7a327
Slama, Aaron <amslama@ilstu.edu>
2019-04-04 14:28:18 -0500

    added the 'add' button which does nothing atm

--- src/alarmClock/AlarmClock.java
+++ src/alarmClock/AlarmClock.java
@@ -1,6 +1,10 @@
 package alarmClock;
 
 import javax.swing.JFrame;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -23,10 +27,16 @@ public class AlarmClock {
 	public static JPanel createGui() 
 	{
 		
-		JPanel mainGui = new JPanel();
+		JPanel mainGui = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		
+		c.gridx = 2;
+		c.gridy = 0;
+
+		JButton addAlarm = new JButton("Add");
+		addAlarm.setBounds(100,100,50,20);
 		
-		
+		mainGui.add(addAlarm);
 		
 		return mainGui;
 	}
