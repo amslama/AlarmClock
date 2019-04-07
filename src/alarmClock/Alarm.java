baaecda956d5ae0abbddb080871df832fce13e94
Bennett Johnson <brjohn4@ilstu.edu>
2019-04-07 16:41:35 -0500

    added localdatetime constructor, added localdatetime object to class

--- src/alarmClock/Alarm.java
+++ src/alarmClock/Alarm.java
@@ -1,21 +1,37 @@
 package alarmClock;
 import java.time.*;
+
 public class Alarm {
 	Instant targetTime;
-	Clock now;
+	LocalDateTime target;
+	
+	// constructor for an alarm object with a number of hours, minutes and seconds until it goes off.
 	public Alarm(long hours, long minutes, long seconds) {
 		targetTime = Clock.systemDefaultZone().instant();
-		targetTime.plusSeconds(hours * 60 * 60);
-		targetTime.plusSeconds(minutes * 60);
-		targetTime.plusSeconds(seconds);
+		targetTime.plusSeconds((hours * 60 * 60) + (minutes * 60) + (seconds));
+		target = LocalDateTime.ofInstant(targetTime, Clock.systemDefaultZone().getZone());
+		
+		
 	}
+	// constructor for an alarm object with a number of minutes and seconds until it goes off.
 	public Alarm(long minutes, long seconds) {
 		targetTime = Clock.systemDefaultZone().instant();
-		targetTime.plusSeconds(minutes * 60);
-		targetTime.plusSeconds(seconds);
+		targetTime.plusSeconds((minutes * 60) + (seconds));
+		target = LocalDateTime.ofInstant(targetTime, Clock.systemDefaultZone().getZone());
 	}
+	// constructor for an alarm object with a number of seconds until it goes off.
 	public Alarm(long seconds) {
 		targetTime = Clock.systemDefaultZone().instant();
 		targetTime.plusSeconds(seconds);
+		target = LocalDateTime.ofInstant(targetTime, Clock.systemDefaultZone().getZone());
+	}
+	// constructor for a LocalDateTime object
+	public Alarm(LocalDateTime time) {
+		target = time;
+	}
+	
+	public boolean expired() {
+		//temp
+		return false;
 	}
 }
