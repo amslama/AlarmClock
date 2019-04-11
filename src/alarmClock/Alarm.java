325c880bf48bd62bf6172808e72f9fdb62240ad4
Bennett Johnson <brjohn4@ilstu.edu>
2019-04-10 20:34:54 -0500

    added message to Alarm and getMessage method

--- src/alarmClock/Alarm.java
+++ src/alarmClock/Alarm.java
@@ -5,33 +5,41 @@ public class Alarm {
 	private Instant targetTime;
 	private LocalDateTime target;
 	private String name;
+	private String message;
 	// constructor for an alarm object with a number of hours, minutes and seconds until it goes off.
-	public Alarm(String n, long hours, long minutes, long seconds) {
+	public Alarm(String n,String m, long hours, long minutes, long seconds) {
 		targetTime = Clock.systemDefaultZone().instant();
 		targetTime = targetTime.plusSeconds((hours * 60 * 60) + (minutes * 60) + (seconds));
 		target = LocalDateTime.ofInstant(targetTime, Clock.systemDefaultZone().getZone());
 		name = n;
+		message = m;
 		
 	}
 	// constructor for an alarm object with a number of minutes and seconds until it goes off.
-	public Alarm(String n, long minutes, long seconds) {
+	public Alarm(String n,String m, long minutes, long seconds) {
 		targetTime = Clock.systemDefaultZone().instant();
 		targetTime = targetTime.plusSeconds((minutes * 60) + (seconds));
 		target = LocalDateTime.ofInstant(targetTime, Clock.systemDefaultZone().getZone());
 		name = n;
+		message = m;
+		
 	}
 	// constructor for an alarm object with a number of seconds until it goes off.
-	public Alarm(String n, long seconds) {
+	public Alarm(String n,String m, long seconds) {
 		targetTime = Clock.systemDefaultZone().instant();
 		targetTime = targetTime.plusSeconds(seconds);
 		
 		target = LocalDateTime.ofInstant(targetTime, Clock.systemDefaultZone().getZone());
 		name = n;
+		message = m;
+		
 	}
 	// constructor for a LocalDateTime object
-	public Alarm(String n, LocalDateTime time) {
+	public Alarm(String n,String m, LocalDateTime time) {
 		target = time;
 		name = n;
+		message = m;
+		
 	}
 	
 	public boolean expired() {
@@ -43,6 +51,9 @@ public class Alarm {
 	}
 	public String getName() {
 		return name;
+	}
+	public String getMessage() {
+		return message;
 	}
 	public String toString() {
 		return target.toString();
